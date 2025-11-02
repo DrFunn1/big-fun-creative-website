93118613a7e425f7e617a4d387d8936774b8c785
DrFunn1 <ed@drfunn.com>
2025-11-02 13:52:59 -0500

    Add footer placeholder for dynamic footer loading

---
+++ edit-bfc-website-np-pluplus.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "H:\Shared drives\Websites\GitHub\big-fun-creative-website"
+start "" "C:\Program Files\Notepad++\notepad++.exe" -multiInst -nosession "index.html" "projects\shrooms.html" "projects\bigpixel.html" "projects\starlords.html" "projects\lucky-13.html" "projects\gmmdr.html" "projects\tunnel.html" "projects\scrabble.html" "projects\table-spiral.html" "projects\slotmachine.html" "assets\css\style.css"
+
+```
+
+**Flags explained:**
+- `-multiInst` = Opens a separate instance
+- `-nosession` = Doesn't load your normal session
+
